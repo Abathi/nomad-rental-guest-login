6486dcc605554638a5841be2035c7c963f1bb56c
Amjad <eng.amjadbathich@gmail.com>
2021-03-28 23:59:42 +0800

    Finish side menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,45 @@
-import logo from '../assets/images/logo.png';
+import logo from "../assets/images/logo.png";
 
 const Header = () => {
-    return (
-        <header className="bg-gray fixed-top">
-            <div className="container-fluid py-2">
-                <div className="row justify-content-between">
-                    <div className="col-6 col-md-2">
-                        <img src={logo} alt="" className="w-100"></img>
-                    </div>
-                    <div className="col- col-md-2 text-right">
-                        <div className="dropdown">
-                            <button className="btn btn-theme p-2" type="button" id="city" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Choose a city
-                            <span className="btn btn-light ml-2">
-                                    <i className="fas fa-bars"></i>
-                                </span>
-                            </button>
-                            <div className="dropdown-menu" aria-labelledby="city">
-                                <a className="dropdown-item" href="">Malaysia</a>
-                                <a className="dropdown-item" href="">China</a>
-                                <a className="dropdown-item" href="">Singapore</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <header className="bg-gray fixed-top">
+      <div className="container-fluid py-2">
+        <div className="row justify-content-between">
+          <div className="col-5 col-md-2 my-auto">
+            <img src={logo} alt="" className="w-100"></img>
+          </div>
+          <div className="col-7 col-md-2 my-auto text-right">
+            <div className="dropdown">
+              <button
+                className="btn btn-theme p-2"
+                type="button"
+                id="city"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Choose a city
+                <span className="btn btn-light ml-2">
+                  <i className="fas fa-bars"></i>
+                </span>
+              </button>
+              <div className="dropdown-menu" aria-labelledby="city">
+                <a className="dropdown-item" href="">
+                  Malaysia
+                </a>
+                <a className="dropdown-item" href="">
+                  China
+                </a>
+                <a className="dropdown-item" href="">
+                  Singapore
+                </a>
+              </div>
             </div>
-        </header>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,39 +1,102 @@
-import Button from '../components/Button'
-import user from '../assets/images/user.png';
-import faq from '../assets/images/faq.png';
-import contact from '../assets/images/contact.png';
-import account from '../assets/images/account.png';
-import refer from '../assets/images/refer.png';
-import toggle from '../assets/images/toggle.png';
+import Button from "../components/Button";
+import user from "../assets/images/user.png";
+import faq from "../assets/images/faq.png";
+import contact from "../assets/images/contact.png";
+import account from "../assets/images/account.png";
+import refer from "../assets/images/refer.png";
+import toggle from "../assets/images/toggle.png";
+import booking from "../assets/images/booking.png";
 
 const SideMenu = () => {
-    return (
-        <div className="col-12 col-md-3 px-0 px-md-2">
-            <div className="row mx-0 mb-3">
-                <div className="col-4 col-md-3 px-0 profile-img position-relative">
-                    <img src={user} alt="" className="w-100"></img>
-                    <span className="position-absolute small-title text-white">
-                        JD
-                    </span>
-                </div>
-            </div>
-            <div className="main-title mb-1">John Doe</div>
-            <div className="sec-title" style={{ opacity: '0.7' }}>
-                Premium Nomad
-                </div>
-            <Button
-                style={{ color: '#168EEA' }}
-                classes={'btn btn-primary w-100'}
-                text={'Booking'}
-            />
-            <div><img src={refer} alt=""></img>Refer and Earn</div>
-            <div><img src={account} alt=""></img>Account Settings</div>
-            <div>Support</div>
-            <div><img src={contact} alt=""></img>Contact us</div>
-            <div><img src={faq} alt=""></img>FAQ</div>
-            <div><img src={toggle} alt="" width="10%"></img>Sign out</div>
+  return (
+    <div className="col-12 col-md-3 px-0 px-md-2">
+      <div className="row mx-0 mb-3 justify-content-center justify-content-md-start">
+        <div className="col-4 col-md-3 px-0 profile-img position-relative">
+          <img src={user} alt="" className="w-100"></img>
+          <span className="position-absolute small-title text-white">JD</span>
         </div>
-    )
-}
+      </div>
+      <div className="main-title mb-1 text-center text-md-left">John Doe</div>
+      <div
+        className="sec-title text-center text-md-left"
+        style={{ opacity: "0.7" }}
+      >
+        Premium Nomad
+      </div>
+      <nav className="navbar navbar-expand-lg">
+        <button
+          className="btn btn-theme bg-color-1 text-white w-100 navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#sideMenuExpand"
+          aria-controls="sideMenuExpand"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          Menu
+          <span className="ml-2">
+            <i className="fas fa-bars"></i>
+          </span>
+        </button>
+        <div className="collapse navbar-collapse" id="sideMenuExpand">
+          <div className="side-menu-options p-2 my-4 active">
+            <div className="row mx-0">
+              <div className="col-1 px-0">
+                <img src={booking} alt="" className="w-100 mr-2"></img>
+              </div>
+              <div className="col-11">Booking</div>
+            </div>
+          </div>
+          <div className="side-menu-options p-2 mb-2">
+            <div className="row mx-0">
+              <div className="col-1 px-0">
+                <img src={refer} alt="" className="w-100 mr-2"></img>
+              </div>
+              <div className="col-11">Refer and Earn</div>
+            </div>
+          </div>
+          <div className="side-menu-options p-2 mb-2">
+            <div className="row mx-0">
+              <div className="col-1 px-0">
+                <img src={account} alt="" className="w-100 mr-2"></img>
+              </div>
+              <div className="col-11">Account Settings</div>
+            </div>
+          </div>
+          <div
+            className="small-title-light mt-4 mb-2"
+            style={{ opacity: "0.7" }}
+          >
+            Support
+          </div>
+          <div className="side-menu-options p-2 mb-2">
+            <div className="row mx-0">
+              <div className="col-1 px-0">
+                <img src={contact} alt="" className="w-100 mr-2"></img>
+              </div>
+              <div className="col-11">Contact us</div>
+            </div>
+          </div>
+          <div className="side-menu-options p-2 mb-5">
+            <div className="row mx-0">
+              <div className="col-1 px-0">
+                <img src={faq} alt="" className="w-100 mr-2"></img>
+              </div>
+              <div className="col-11">FAQ</div>
+            </div>
+          </div>
+          <div className="side-menu-options p-2 pt-5 mt-t">
+            <div className="row mx-0">
+              <div className="col-1 px-0">
+                <img src={toggle} alt="" className="w-100 mr-2"></img>
+              </div>
+              <div className="col-11">Sign out</div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default SideMenu
+export default SideMenu;
