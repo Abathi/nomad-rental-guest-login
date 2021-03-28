6d6e1f31ee5560daa110d33f54e2715f9e572f13
Amjad <eng.amjadbathich@gmail.com>
2021-03-28 19:04:37 +0800

    sign up

---
+++ src/components/SignUp.js
@@ -0,0 +1,14 @@
+import SideMenu from '../components/SideMenu'
+
+const SignUp = () => {
+    return (
+        <div>
+            <div className="row">
+                <div className="col-3"><SideMenu /></div>
+                <div className="col-9"></div>
+            </div>
+        </div>
+    )
+}
+
+export default SignUp
