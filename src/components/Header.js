9b55780d38aca34ca3fe204b986e8ba27e53bb34
Amjad <eng.amjadbathich@gmail.com>
2021-03-28 22:52:51 +0800

    header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,24 @@ import logo from '../assets/images/logo.png';
 
 const Header = () => {
     return (
-        <header>
-            <div className="row mb-3 mt-5">
-                <div className="col-12 col-md-2 mb-3 mb-md-0">
+        <header className="container-fluid py-2 py-ms-3">
+            <div className="row justify-content-between">
+                <div className="col-6 col-md-2">
                     <img src={logo} alt="" className="w-100"></img>
                 </div>
-                <div className="col-0 col-md-7"></div>
-                <div className="dropdown">
-                    <button className="p-2" type="button" id="city" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Choose a city
-                        <span className="btn btn-light ml-2"><i className="fas fa-bars"></i></span>
-                    </button>
-                    <div className="dropdown-menu" aria-labelledby="city">
-                        <a className="dropdown-item" href="">Malaysia</a>
-                        <a className="dropdown-item" href="">China</a>
-                        <a className="dropdown-item" href="">Singapore</a>
+                <div className="col- col-md-2 text-right">
+                    <div className="dropdown">
+                        <button className="btn btn-theme p-2" type="button" id="city" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Choose a city
+                            <span className="btn btn-light ml-2">
+                                <i className="fas fa-bars"></i>
+                            </span>
+                        </button>
+                        <div className="dropdown-menu" aria-labelledby="city">
+                            <a className="dropdown-item" href="">Malaysia</a>
+                            <a className="dropdown-item" href="">China</a>
+                            <a className="dropdown-item" href="">Singapore</a>
+                        </div>
                     </div>
                 </div>
             </div>
