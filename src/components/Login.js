d2f1dd84123198a07d0b3f32d0995b4ba4bd0d37
Amjad <eng.amjadbathich@gmail.com>
2021-03-29 01:53:10 +0800

    Finish login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,45 +1,76 @@
-import Button from './Button'
-import key from '../assets/images/key.png';
-import login from '../assets/images/login.jpg';
+import Button from "./Button";
+import key from "../assets/images/key.png";
+import login from "../assets/images/login.jpg";
 
 const Login = () => {
-    return (
-        <div>
-            <div className="row">
-                <div className="col-12 col-md-4 pr-0">
-                    <img src={key} alt="" className="mt-5"></img>
-                    <div>Hi there!</div>
-                    <div>Sign in to manage your listings and booking requests on Normal Rental</div>
-                    <div className="blue-link">{'>'} Learn more about our premium subscription</div>
-                    <div style={{ background: '#f5f6f8' }}>
-                        <form>
-                            <div className="form-group">
-                                <label for="email">Email address</label>
-                                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Email" />
-                            </div>
-                            <div className="form-group">
-                                <label for="password">Password</label>
-                                <span className="float-right blue-link">Forgot your password?</span>
-                                <input type="password" className="form-control" id="password" placeholder="Password" />
-                            </div>
-                            <Button
-                                classes={'btn btn-primary w-100'}
-                                text={'Sign in'}
-                            />
-                        </form>
-                        <div className="line"></div>
-                        <div style={{ textAlign: 'center' }}>
-                            <span>Don't have an account? </span>
-                            <span className="blue-link">Sign up now</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-12 col-md-8 pl-0">
-                    <img src={login} alt="" className="w-100"></img>
-                </div>
+  return (
+    <div>
+      <div className="row">
+        <div className="col-12 col-md-4 pr-0">
+          <div className="mx-5">
+            <img src={key} alt="" className="mt-5"></img>
+            <h2 className="main-title mb-3">Hi there!</h2>
+            <div className="main-desc mb-3">
+              Sign in to manage your listings and booking requests on Normal
+              Rental
             </div>
+            <a href="./" className="d-block small-title color-2 mb-3">
+              {">"} Learn more about our premium subscription
+            </a>
+            <form>
+              <div className="form-group">
+                <label htmlFor="email">Email address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  aria-describedby="emailHelp"
+                  placeholder="Email"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <a
+                  href="./"
+                  className="float-right d-block small-title color-2"
+                >
+                  Forgot your password?
+                </a>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="Password"
+                />
+              </div>
+              <Button
+                classes={"btn btn-primary w-100 mb-2"}
+                text={"Sign in"}
+                style={{ background: "#786AFF" }}
+              />
+            </form>
+            <div
+              className="w-divider w-100 my-3"
+              style={{
+                height: "0.5px",
+                backgroundColor: "#000",
+                opacity: "0.05",
+              }}
+            ></div>
+            <div style={{ textAlign: "center" }}>
+              <span>Don't have an account?</span>
+              <a href="./" className="small-title color-2">
+                Sign up now
+              </a>
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className="col-12 col-md-8 pl-0">
+          <img src={login} alt="" className="w-100"></img>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
