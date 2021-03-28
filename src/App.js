bee4b82c250157453f12f74669bbe1750dba4443
Amjad <eng.amjadbathich@gmail.com>
2021-03-28 19:31:38 +0800

    route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Login from './components/Login'
 import Booking from './components/Booking'
+import Account from './components/Account'
 
 
 function App() {
   return (
     <Router>
-      <div className="container">
-        <Login />
-        <Route path='/booking' component={Booking} />
-      </div>
+      <Route
+        path='/'
+        exact
+        render={() => (
+          <Login />
+        )}
+      />
+      <Route path='/booking' component={Booking} />
+      <Route path='/account' component={Account} />
     </Router>
   );
 }
