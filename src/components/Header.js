79f5fa531bde1464d98d2e68fd89d96eb32fc811
Amjad <eng.amjadbathich@gmail.com>
2021-03-28 16:11:21 +0800

    login page

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,15 +8,15 @@ const Header = () => {
                     <img src={logo} alt="" className="w-100"></img>
                 </div>
                 <div className="col-0 col-md-7"></div>
-                <div className="col-12 col-md-3 mb-3 mb-md-0">
-                    Choose a city
-                    <div className="btn-group">
-                        <button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false"></button>
-                        <ul className="dropdown-menu dropdown-menu-lg-end">
-                            <li><button className="dropdown-item" type="button">Action</button></li>
-                            <li><button className="dropdown-item" type="button">Another action</button></li>
-                            <li><button className="dropdown-item" type="button">Something else here</button></li>
-                        </ul>
+                <div className="dropdown">
+                    <button className="p-2" type="button" id="city" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Choose a city
+                        <span className="btn btn-light ml-2"><i className="fas fa-bars"></i></span>
+                    </button>
+                    <div className="dropdown-menu" aria-labelledby="city">
+                        <a className="dropdown-item" href="">Malaysia</a>
+                        <a className="dropdown-item" href="">China</a>
+                        <a className="dropdown-item" href="">Singapore</a>
                     </div>
                 </div>
             </div>
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,29 +1,44 @@
 import Button from './Button'
+import key from '../assets/images/key.png';
+import login from '../assets/images/login.jpg';
 
 const LoginForm = () => {
     return (
-        <div style={{ background: '#f5f6f8' }}>
-            <form>
-                <div className="form-group">
-                    <label for="email">Email address</label>
-                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Email" />
+        <div>
+            <div className="row">
+                <div className="col-12 col-md-4 pr-0">
+                    <img src={key} alt="" className="mt-5"></img>
+                    <div>Hi there!</div>
+                    <div>Sign in to manage your listings and booking requests on Normal Rental</div>
+                    <div className="blue-link">{'>'} Learn more about our premium subscription</div>
+                    <div style={{ background: '#f5f6f8' }}>
+                        <form>
+                            <div className="form-group">
+                                <label for="email">Email address</label>
+                                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Email" />
+                            </div>
+                            <div className="form-group">
+                                <label for="password">Password</label>
+                                <span className="float-right blue-link">Forgot your password?</span>
+                                <input type="password" className="form-control" id="password" placeholder="Password" />
+                            </div>
+                            <Button
+                                classes={'btn btn-primary w-100'}
+                                text={'Sign in'}
+                            />
+                        </form>
+                        <div className="line"></div>
+                        <div style={{ textAlign: 'center' }}>
+                            <span>Don't have an account? </span>
+                            <span className="blue-link">Sign up now</span>
+                        </div>
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label for="password">Password</label>
-                    <span className="float-right" style={{ color: '#4aa8f1', cursor: 'pointer' }}>Forgot your password?</span>
-                    <input type="password" className="form-control" id="password" placeholder="Password" />
+                <div className="col-12 col-md-8 pl-0">
+                    <img src={login} alt="" className="w-100"></img>
                 </div>
-                <Button
-                    classes={'btn btn-primary w-100'}
-                    text={'Sign in'}
-                />
-            </form>
-            <div className="line"></div>
-            <div style={{ textAlign: 'center' }}>
-                <span>Don't have an account? </span>
-                <span style={{ color: '#4aa8f1', cursor: 'pointer' }}>Sign up now</span>
             </div>
-        </div >
+        </div>
     )
 }
 
